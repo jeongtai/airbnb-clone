f977e8e6732ab5bbc1d830f79e4ed192d29900ee
jeongtai <zzze37@naver.com>
2020-06-25 19:34:08 +0900

    #3.4 Falling in Love with Admin Panel

--- config/settings.py
+++ config/settings.py
@@ -117,4 +117,3 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 AUTH_USER_MODEL = "users.User"
-
--- users/admin.py
+++ users/admin.py
@@ -3,7 +3,18 @@ from . import models
 
 # Register your models here.
 
+# admin.site.register(models.User, CustomUserAdmin)
+# 위랑 아래랑 같은 뜻임. admin 패널에서 이 user를 볼 거다.
+
 
 @admin.register(models.User)
 class CustomUserAdmin(admin.ModelAdmin):
-    pass
+
+    """ Custom User Admin """
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
