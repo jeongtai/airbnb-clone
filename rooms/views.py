c08c0a764879cf9cf2adb3e784022e5449fb55a9
jeongtai <zzze37@naver.com>
2020-07-17 05:10:34 +0900

    #10.2 Introduction to Django Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def all_rooms(request):
-    return HttpResponse(content="hello")
+    return render(request, "all_rooms.html")
 
